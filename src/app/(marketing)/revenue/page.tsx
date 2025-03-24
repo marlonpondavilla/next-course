4fc44de7ba58517b36358fe81a314b4347d916fc
MarlonDev <marlonpondavilla@dev.com>
2025-03-24 11:48:36 +0800

    Remove global CSS import from auth and marketing layouts; add marketing layout and home page

--- src/app/(marketing)/revenue/page.tsx
+++ src/app/(marketing)/revenue/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Revenue() {
   return (
-    <div>
+    <div className='flex justify-center items-center h-screen bg-gray-500'>
       <h1>Revenue</h1>
     </div>
   )

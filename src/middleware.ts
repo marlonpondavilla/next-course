9a2b3ce81c48e2691cd00b00c4e7ededf5c71e25
MarlonDev <marlonpondavilla@dev.com>
2025-03-26 21:50:35 +0800

    Add middleware to redirect profile route to home

---
+++ src/middleware.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+import type {NextRequest} from "next/server";
+
+export function middleware(request: NextRequest) {
+  
+  if(request.nextUrl.pathname === '/profile'){
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+}
+
+export const config = {
+  matcher: [
+    "/",
+    "/profile"
+  ]
+
+}

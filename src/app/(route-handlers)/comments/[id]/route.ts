938be843d5cfc7cf69e1ce6cf4ca2fbd821aa83e
MarlonDev <marlonpondavilla@dev.com>
2025-03-26 11:19:37 +0800

    Add PATCH route handler for updating a comment by ID

--- src/app/(route-handlers)/comments/[id]/route.ts
+++ src/app/(route-handlers)/comments/[id]/route.ts
@@ -9,3 +9,17 @@ export async function GET(
 
     return Response.json(comment);
 }
+
+export async function PATCH(
+  request: Request, 
+  {params}: {params: Promise<{id: string}>})
+  {
+    const {id} = await params;
+    const body = await request.json();
+    const {text} = body;
+
+    const index = commentsData.findIndex((comment) => comment.id === parseInt(id));
+    commentsData[index].text = text;
+
+    return Response.json(commentsData[index]);
+}

3b2643cdbfa686746a1db01ea73b58a3828de908
MarlonDev <marlonpondavilla@dev.com>
2025-03-26 11:00:14 +0800

    Add comments route handler with GET and POST functionality

---
+++ src/app/(route-handlers)/comments/data.ts
@@ -0,0 +1,14 @@
+export const commentsData = [
+  {
+    id: 1,
+    text: 'This is a comment'
+  },
+  {
+    id: 2,
+    text: 'This is another comment'
+  },
+  {
+    id: 3,
+    text: 'This is yet another comment'
+  }
+]

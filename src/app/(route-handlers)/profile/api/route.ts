db8ebf624973524f5e1cf8b80541a1fe5984eaa0
MarlonDev <marlonpondavilla@dev.com>
2025-03-26 14:59:57 +0800

    Enhance profile route handler to manage cookies and set response headers

--- src/app/(route-handlers)/profile/api/route.ts
+++ src/app/(route-handlers)/profile/api/route.ts
@@ -1,4 +1,4 @@
-import { headers } from "next/headers";
+import { headers, cookies } from "next/headers";
 import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest){
@@ -8,5 +8,19 @@ export async function GET(request: NextRequest){
   const headerList = await headers();
   console.log(headerList.get('Authorization')); 
 
-  return new Response('Profile route handler');
+  const theme = request.cookies.get('theme');
+  console.log(theme);
+
+  const cookieStore = await cookies();
+  cookieStore.set('page', '20')
+  console.log(cookieStore.get('page'));
+  return new Response(
+    'Profile route handler', 
+    {
+      headers: {
+        'Content-Type': 'text/plain',
+        'Set-Cookie': 'theme=dark'
+      }
+    }
+);
 }

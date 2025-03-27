011ba0807b035bedf4b061c080047bc099cfe122
MarlonDev <marlonpondavilla@dev.com>
2025-03-27 17:23:38 +0800

    Initialize Prisma setup with Product model and seed data

---
+++ src/app/(database)/products-db/page.tsx
@@ -0,0 +1,27 @@
+import { getProducts } from "@/prisma-db"
+import { Decimal } from "@prisma/client/runtime/client";
+
+
+type Product = {
+  id: number;
+  title: string;
+  price: Decimal;
+  description: string | null;
+}
+
+export default async function ProductDBPage(){
+  const products: Product[] = await getProducts();
+
+  return(
+    <ul className="space-x-4 space-y-4 p-4">
+      {products.map((product) => (
+        <li key={product.id} className="p-4 border border-gray-300 rounded space-y-4">
+          <h2 className="text-lg font-semibold">{product.title}</h2>
+          <p className="text-sm">{product.description}</p>
+          <p className="text-base text-blue-400">P{product.price.toFixed(2)}</p>
+        </li>
+      ))}
+    </ul>
+  )
+}
+

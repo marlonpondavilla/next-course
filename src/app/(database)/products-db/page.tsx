dbeeaacfe07bd2fe075975d49f4e45b2413b770d
MarlonDev <marlonpondavilla@dev.com>
2025-03-28 21:32:30 +0800

    Refactor product display and delete functionality into a new ProductDetail component

--- src/app/(database)/products-db/page.tsx
+++ src/app/(database)/products-db/page.tsx
@@ -1,6 +1,5 @@
-import { deleteProductAction } from "@/actions/products";
 import { getProducts } from "@/prisma-db"
-import Link from "next/link";
+import { ProductDetail } from "./products-detail";
 
 export type Product = {
   id: number;
@@ -9,26 +8,11 @@ export type Product = {
   description: string | null;
 }
 
+
 export default async function ProductDBPage(){
   const products: Product[] = await getProducts();
-
   return(
-    
-      <ul className="space-x-4 space-y-4 p-4 m-8 flex flex-wrap justify-center items-center">
-        {products.map((product) => (
-          <li key={product.id} className="p-4 border border-gray-300 rounded space-y-4 w-1/2">
-            <h2 className="text-lg font-semibold">
-              <Link href={`/products-db/${product.id}`}>{product.title}</Link>
-            </h2>
-            <p className="text-sm">{product.description}</p>
-            <p className="text-base text-blue-400">P{product.price.toString()}</p>
-            <form action={deleteProductAction.bind(null, product.id)}>
-              <button className="px-4 py-2 text-white bg-red-500 rounded-md hover:bg-red-400">Delete</button>
-            </form>
-          </li>
-        ))}
-      </ul>
-    
+    <ProductDetail products={products} />
   )
 }
 
---
+++ src/app/(database)/products-db/products-detail.tsx
@@ -0,0 +1,47 @@
+"use client";
+import { useOptimistic } from "react";
+import { deleteProductAction } from "@/actions/products";
+import { getProducts } from "@/prisma-db"
+import Link from "next/link";
+
+export type Product = {
+  id: number;
+  title: string;
+  price: bigint;
+  description: string | null;
+}
+
+
+export function ProductDetail({products}: {products: Product[]}){
+  const [optimisticProucts, setOptimisticProducts] = useOptimistic(
+    products,
+    (currentProducts, productId) => {
+      return currentProducts.filter((product) => product.id !== productId);
+    }
+  );
+
+  const removeProductById = async (productId: number) => {
+    setOptimisticProducts(productId);
+    await deleteProductAction(productId);
+  }
+
+  return(
+    
+      <ul className="space-x-4 space-y-4 p-4 m-8 flex flex-wrap justify-center items-center">
+        {optimisticProucts.map((product) => (
+          <li key={product.id} className="p-4 border border-gray-300 rounded space-y-4 w-1/2">
+            <h2 className="text-lg font-semibold">
+              <Link href={`/products-db/${product.id}`}>{product.title}</Link>
+            </h2>
+            <p className="text-sm">{product.description}</p>
+            <p className="text-base text-blue-400">P{product.price.toString()}</p>
+            <form action={removeProductById.bind(null, product.id)}>
+              <button className="px-4 py-2 text-white bg-red-500 rounded-md hover:bg-red-400">Delete</button>
+            </form>
+          </li>
+        ))}
+      </ul>
+    
+  )
+}
+
